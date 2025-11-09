80497bfca86dae0da520a3fddc5401e41ba539b5
Pedro <pedrohg2609@gmail.com>
2025-11-09 01:47:48 -0300

    adicoes de workflows pylint

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  
-from .routers import auth
+from .routers import users, auth
 
 app = FastAPI(
     title="GenT API",
