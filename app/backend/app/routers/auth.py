5f35151b80764946efeb9624e8d83da9ccdda6bf
Pedro <pedrohg2609@gmail.com>
2025-11-10 17:31:26 -0300

    adiciona o endpoint da home junto com parte dos models das perguntas

--- app/backend/app/routers/auth.py
+++ app/backend/app/routers/auth.py
@@ -46,14 +46,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(dependencies.get
 
     if db_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Email ja registrado"
-        )
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Email ja registrado" 
+    )
 
-    empresa_nome = getattr(user, "empresa", None)
-    if empresa_nome:
-        db_empresa = crud.get_or_create_empresa(db, nome=empresa_nome)
-        empresa_id = db_empresa.id
-    else:
-        empresa_id = None
+    db_empresa = crud.get_or_create_empresa(db, nome = user.empresa)
+    
 
-    return crud.create_user(db=db, user=user, empresa_id=empresa_id)
+    return crud.create_user(db = db, user = user, empresa_id = db_empresa.id)
+
