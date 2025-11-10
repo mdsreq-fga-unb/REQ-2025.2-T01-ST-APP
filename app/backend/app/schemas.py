8bf6a12bc4cc584f15202f7b0338bb47b0b1dd1c
Pedro <pedrohg2609@gmail.com>
2025-11-10 19:03:15 -0300

    fix: correcoes de conflito de merge

--- app/backend/app/schemas.py
+++ app/backend/app/schemas.py
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
 from pydantic import BaseModel
-from .models import UserRole
-
-
-class UserBase(BaseModel):
-
-    email: str
-    nome: str
-    role: UserRole = UserRole.Colaborador
-    empresa: str
-    cargo: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-=======
-from pydantic import BaseModel, EmailStr
-from typing import Dict
 from .models import UserRole
 
 class UserBase(BaseModel):
@@ -32,7 +12,6 @@ class UserCreate(UserBase):
     password:str
 
     
->>>>>>> 1db8677 (adiciona cadastro de usuarios)
 class User(UserBase):
     id: int
     is_active: bool
@@ -40,10 +19,6 @@ class User(UserBase):
     class Config:
         from_attributes = True
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1db8677 (adiciona cadastro de usuarios)
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -51,20 +26,11 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     email: str | None = None
-<<<<<<< HEAD
-=======
-
 
 class Home(BaseModel):
-
+    
     nome: str
     cargo: str | None = None
-
+    
     class Config:
         from_attributes = True
-
-
-    
-
-    
->>>>>>> 1db8677 (adiciona cadastro de usuarios)
